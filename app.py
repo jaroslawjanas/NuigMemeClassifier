7a94d1ba281de143ec755366dc1a1cc74e48ae1b
unknown <ertagonn@gmail.com>
2021-02-14 00:40:46 +0000

    Load and resize the image Basic project structure Added requirements.txt Updated .gitignore

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
-print("Hello World!")
+from image_processing.image_processing import *
+
+img_path = '../9gagScrapperJS_v3.0/data/part_19-1-2021_23h8m/images/a1rZQxG.jpg'
+img = image_processing(img_path)
