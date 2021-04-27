270840909c41f41b0678314c8031b3412826a0fe
jaroslawjanas <jaroslawjanas.dev@gmail.com>
2021-04-27 15:16:15 +0100

    Basic implementation of text OCR using Tesseract-OCR

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from text_detection.text_detection import *
 from text_recognition.text_recognition import *
+import yaml
+
+config = ""
+with open("config.yaml", 'r') as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
 
 # img_path = '../9gagScrapperJS_v3.0/data/part_19-1-2021_23h8m/images/a1rZQxG.jpg'
-for i in range(6, 17):
+for i in range(1, 17):
     img_path = 'test_images/test' + str(i) + '.jpg'
     raw_image = cv2.imread(img_path)
     bounding_boxes = text_detection(raw_image)
-    text_recognition(bounding_boxes, raw_image)
+    text_recognition(bounding_boxes, raw_image, config)
 
 
--- text_recognition/text_recognition.py
+++ text_recognition/text_recognition.py
@@ -1,19 +1,23 @@
 import cv2
+import pytesseract
 
+def text_recognition(bounding_boxes, raw_image, config):
+    # path to tesseract-ocr
+    pytesseract.pytesseract.tesseract_cmd = config['tesseract']['path']
 
-def text_recognition(bounding_boxes, raw_image):
     for box in bounding_boxes:
         p1 = box[0]
         p3 = box[2]
         # copy part of image with text
-        print(p1[1], p3[1], p1[0], p3[0])
         text_image = raw_image[int(p1[1]):int(p3[1]), int(p1[0]):int(p3[0])]
 
         cv2.imshow("Text image", text_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-        text_image_processing(text_image)
+        processed_image = text_image_processing(text_image)
+        text = pytesseract.image_to_string(processed_image, 'eng', '')
+        print(text)
 
     return None
 
@@ -23,7 +27,7 @@ def text_image_processing(text_image):
 
     processed_image = cv2.cvtColor(text_image, cv2.COLOR_BGR2GRAY)
     if width > 20 and height > 20:
-        processed_image = cv2.GaussianBlur(processed_image, (3, 3), 0)
+        processed_image = cv2.GaussianBlur(processed_image, (5, 5), 0)
 
     cv2.imshow("Text image", processed_image)
     cv2.waitKey(0)
@@ -32,10 +36,11 @@ def text_image_processing(text_image):
     processed_image = cv2.adaptiveThreshold(processed_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 5)
     # _, processed_image = cv2.threshold(processed_image, 230, 255, cv2.THRESH_BINARY)
 
-    # processed_image = cv2.erode(processed_image, (7, 7), iterations=3)
-
+    # processed_image = cv2.dilate(processed_image, (3, 3), iterations=1)
+    # processed_image = cv2.erode(processed_image, (3, 3), iterations=2)
+    #
     cv2.imshow("Text image", processed_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    return None
+    return processed_image
