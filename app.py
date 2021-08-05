f6418bed86d49c1e1767cff6efb1efd627661ab9
jaroslawjanas <jaroslawjanas.dev@gmail.com>
2021-08-05 02:30:50 +0100

    Allow for multiple images in template folders Code cleanup

--- app.py
+++ app.py
@@ -11,16 +11,13 @@ with open("config.yaml", 'r') as stream:
     except yaml.YAMLError as exc:
         print(exc)
 
-for i in range(11, 17):
+for i in range(2, 15):
     input_img_path = 'test_images/test' + str(i) + '.jpg'
     input_img = cv2.imread(input_img_path)
     bounding_boxes = text_detection(input_img, config)
     text_recognition(bounding_boxes, input_img, config)
 
-
-
     templates = os.listdir(config['data']['templates']['path'])
-    templatePaths = [config['data']['templates']['path'] + p + '\\' + p+ '.jpg' for p in templates]
+    template_paths = [config['data']['templates']['path'] + p + '\\' for p in templates]
 
-    image_matching(input_img, bounding_boxes, templatePaths)
-
+    image_matching(input_img, bounding_boxes, templates, config)
--- image_classification/image_similarity.py
+++ image_classification/image_similarity.py
@@ -1,8 +1,9 @@
 import os
 import cv2
 
+
 # Based on https://docs.opencv.org/4.5.2/dc/dc3/tutorial_py_matcher.html
-def image_matching(img, bounding_boxes, templatePaths):
+def image_matching(img, bounding_boxes, templates, config):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # lets cover text with white filled boxes
@@ -18,39 +19,46 @@ def image_matching(img, bounding_boxes, templatePaths):
     cv2.destroyAllWindows()
 
     orb = cv2.ORB_create(nfeatures=700)
-    keypoints, descriptors = orb.detectAndCompute(img, None)
+    input_img_key_points, input_img_descriptors = orb.detectAndCompute(img, None)
 
-    bfmatcher = cv2.BFMatcher()
-    best_match = [0, None, None, None]
-    for tempPath in templatePaths:
-        tempImg = cv2.imread(tempPath, cv2.IMREAD_GRAYSCALE)
-        # print(tempPath)
-        # print(tempImg)
-        t_keypoints, t_descriptors = orb.detectAndCompute(tempImg, None)
-        if t_keypoints is None or t_descriptors is None:
-            continue
+    bf_matcher = cv2.BFMatcher()
+    best_match = {
+        'matching_features': [],
+        'template_name': None,
+        'template_img': None,
+    }
 
-        matches = bfmatcher.knnMatch(descriptors, t_descriptors, k=2)
-        matching_features = []
-        for a, b in matches:
-            if a.distance < 0.75 * b.distance:
-                matching_features.append([a])
+    for template_name in templates:
+        template_dir_path = os.path.join(config['data']['templates']['path'], template_name)
+        for file_name in os.listdir(template_dir_path):
+            file_path = os.path.join(template_dir_path, file_name)
 
-        if len(matching_features) > best_match[0]:
-            best_match[0] = len(matching_features)
-            best_match[1] = tempPath
-            best_match[2] = tempImg
-            best_match[3] = matching_features
+            template_img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+            template_key_points, template_descriptors = orb.detectAndCompute(template_img, None)
+            if template_key_points is None or template_descriptors is None:
+                continue
 
-    if best_match[0] == 0:
+            matches = bf_matcher.knnMatch(input_img_descriptors, template_descriptors, k=2)
+            matching_features = []
+            for a, b in matches:
+                if a.distance < 0.75 * b.distance:
+                    matching_features.append([a])
+
+            if len(matching_features) > len(best_match['matching_features']):
+                best_match['matching_features'] = matching_features
+                best_match['template_name'] = template_name
+                best_match['template_img'] = template_img
+
+    if len(best_match['matching_features']) == 0:
         print('A matching template could not be found')
     else:
-        if best_match[0] < 20:
+        if len(best_match['matching_features']) < 20:
             print('The matching template might be incorrect')
 
-        print('Template: ' + best_match[1].split('\\').pop().split('.')[0])
-        t_keypoints, t_descriptors = orb.detectAndCompute(best_match[2], None)
-        img5 = cv2.drawMatchesKnn(img, keypoints, best_match[2], t_keypoints, best_match[3], None, flags=2)
+        print('Template: ' + best_match['template_name'])
+        template_key_points, template_descriptors = orb.detectAndCompute(best_match['template_img'], None)
+        img5 = cv2.drawMatchesKnn(img, input_img_key_points, best_match['template_img'],
+                                  template_key_points, best_match['matching_features'], None, flags=2)
         cv2.imshow('', img5)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
